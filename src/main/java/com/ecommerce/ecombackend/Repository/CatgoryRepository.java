2d1f125605dc60ddf1802cd88159e78be6c80720
“krishna787991” <patelkrishna7879@gmail.com>
2024-10-19 00:31:33 +0530

    CategoryApi With h2 And JPA

---
+++ src/main/java/com/ecommerce/ecombackend/Repository/CatgoryRepository.java
@@ -0,0 +1,8 @@
+package com.ecommerce.ecombackend.Repository;
+
+import com.ecommerce.ecombackend.Model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CatgoryRepository extends JpaRepository<Category,Long> {
+
+}
--- src/main/java/com/ecommerce/ecombackend/Services/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/ecombackend/Services/CategoryServiceImpl.java
@@ -2,8 +2,12 @@ package com.ecommerce.ecombackend.Services;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import com.ecommerce.ecombackend.Repository.CatgoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -12,46 +16,36 @@ import com.ecommerce.ecombackend.Model.Category;
 @Service
 public class CategoryServiceImpl implements CategoryService {
 
-	private long nextval=1L;
+	@Autowired
+	private CatgoryRepository categoryRepo;
 
-	private List<Category> categories=new ArrayList<>();
-	
 	@Override
 	public List<Category> getAllCategory() {
-		return categories;
+		return categoryRepo.findAll();
 	}
 
 	@Override
 	public String saveCategory(Category category) {
-		category.setCategoryId(nextval++);
-		System.out.println(nextval);
-		categories.add(category);
+		categoryRepo.save(category);
 		return "category added successfully";
 	}
 	
 	
 	public String deleteCategory(Long categoryId) {
-		Category foundCategory = categories.stream()
-			    .filter(c -> c.getCategoryId() == categoryId)
-			    .findFirst()
-			    .orElseThrow(() -> new ResponseStatusException(
-		                HttpStatus.NOT_FOUND, "Category not found"));
-		categories.remove(foundCategory);
+		Category existingCategory=categoryRepo.findById(categoryId).
+				orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND,"Invalid Category ID"));
+		categoryRepo.delete(existingCategory);
 		return "Category deleted succesfully";
 	}
 
 	@Override
 	public String updateCategory(Category category, Long categoryId) {
 		// TODO Auto-generated method stub
-		Category existingCategory=categories.stream().
-		filter(c->c.getCategoryId()==categoryId)
-		.findFirst()
-		.orElseThrow(() -> new ResponseStatusException(
-                HttpStatus.NOT_FOUND, "Invalid categoryId"));
+		Category existingCategory=categoryRepo.findById(categoryId).
+				orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND,"Invalid Category ID"));
 		existingCategory.setCategoryName(category.getCategoryName());
-		
+		categoryRepo.save(existingCategory);
 		return "Category Updated Successfully";
-
 	}
 
 }
